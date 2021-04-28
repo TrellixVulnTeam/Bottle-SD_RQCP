14612e15028e7f0ff36830e3b4e3006a2882da78
vivavomvi <mwazoski96@yahoo.com>
2021-04-28 21:10:17 +0200

    Beta 1.0 - Comprobar errores

--- source/run.py
+++ source/run.py
@@ -4,7 +4,8 @@ from bottle import route,run, template, request
 import mako
 
 USER_INFO = ['','','']
-
+USER_NAME = ['']
+ASIG_LIST = []
 
 @route('/')
 def index():
@@ -20,12 +21,32 @@ def index():
                 Información relativa al usuario que proviene del login
     """
     
-    if USER_INFO[0] == 'admin':
-        admin = 1
-    else:
-        admin = 0
+    try:
+        correo = request.forms.get('mail')
+        password = request.forms.get('password')
 
-    return template('index', count = 0, admin = admin, user = USER_INFO)
+        conn = sqlite3.connect('bottle.db')
+        c = conn.cursor()
+
+        
+        query = "SELECT nombre FROM asignatura WHERE 1=1;"
+        print('\n'+query+'\n')
+        
+        c.execute(query)
+        result = c.fetchall()
+        c.close()       
+        ASIG_LIST = result
+
+
+    except sqlite3.Error as error:
+        print("Error en la consulta",error)
+
+    finally:
+        if (conn):
+            conn.close()
+            print("Conexion Cerrada")
+
+    return template('index', count = 0, admin = 0, user = USER_INFO , asignatura = result)
 
 
 @route('/search_done', method='POST')
@@ -61,6 +82,7 @@ def search_done():
         departamento = request.forms.get('departamento')
         categoria = request.forms.get('categoria')
         dni = request.forms.get('dni')
+        asignatura = request.forms.get('asignatura')
 
         conn = sqlite3.connect('bottle.db')
         c = conn.cursor()
@@ -79,12 +101,20 @@ def search_done():
         if (departamento != '1'):
             query = query + " and departamento = '{}' ".format(departamento)
         
-        
         query = query + " except SELECT nombre,categoria,departamento,correo from usuarios WHERE nombre='admin';"
         print('\n'+query+'\n')
-        
+
+
+        if (asignatura != '1'):
+            
+            query = "select nombre,categoria,departamento,correo FROM usuarios where nombre IN (select profesor from prof_asig where asignatura='{}');".format(asignatura)
+
         c.execute(query)
         result = c.fetchall()
+
+        c.execute("SELECT nombre FROM asignatura WHERE 1=1;")
+        ASIG_LIST = c.fetchall()
+        
         c.close()
 
     except sqlite3.Error as error:
@@ -101,7 +131,7 @@ def search_done():
     else:
         admin = 0
     
-    return template('index', count = 1, resultado = result, admin = admin, user = USER_INFO )
+    return template('index', count = 1, asignatura = ASIG_LIST ,resultado = result, admin = admin, user = USER_INFO )
 
 
 @route('/login', method = 'GET')
@@ -246,35 +276,81 @@ def do_register():
         dni = request.forms.get('dni')
         departamento = request.forms.get('departamento')
         categoria = request.forms.get('categoria')
+        asignatura = request.forms.getlist('asignatura')
 
         conn = sqlite3.connect('bottle.db')
         c = conn.cursor()
 
-        if (name != '' and dni != '' and correo != '' and password != '' and categoria != '1' and departamento != '1'):
+        print(name)
+        print(correo)
+        print(dni)
+        print(departamento)
+
+        if ((name != '' and name != 'admin' and name != None) and dni != '' and correo != '' and password != '' and categoria != '1' and departamento != '1'):
 
             command = "INSERT INTO usuarios (nombre,dni,correo,departamento,categoria,pass) VALUES ('{}' , '{}' , '{}' , '{}' , '{}' , '{}');".format(
-                name, dni, correo, departamento, categoria, password)
+            name, dni, correo, departamento, categoria, password)
 
-        else:
-            return template('register', count = 0 , error = 'Falta algun campo')
+            print("HOLA")
+            USER_NAME[0] = name
+            print(USER_NAME[0])
+            c.execute(command)
+            conn.commit()
+            
+            if (categoria == 'PDI'):
+                c.execute("SELECT nombre FROM asignatura;")
+                asignatura = c.fetchall()
+                return template('register', count = 2, error = '', asignatura = asignatura)
+            else:
+                return template('register', count = 1, error = '')
         
-        c.execute(command)
-        conn.commit()
+        else:
+            print(USER_NAME[0])
+            if asignatura != None:
+                for i in asignatura:
+                    print("marca de base de datos")
+                    command = "INSERT INTO prof_asig (profesor,asignatura) VALUES ('{}','{}')".format(USER_NAME[0], i)
+                    c.execute(command)
+                    conn.commit()
+                return template("register", count = 1, error='')
+
+            else:
+                return template('register', count = 0 , error = 'Falta algun campo')
+        
         c.close()
 
-    except sqlite3.Error as error:
-            print("Error mientras se insertaban los datos",error)
+    except sqlite3.Error as m_error:
+            print("Error mientras se insertaban los datos",m_error)
+            return template('register', count = 0 , error = 'Error Base de Datos')
+
 
     finally:
             if (conn):
                 conn.close()
                 print("Conexion Cerrada")
-    
-    return template('register', count = 1, error = '')
-
 
 @route('/modify')
 def modify():
+
+    """
+    Ruta que solo se activa si eres admin y que sirve para modificar la información, excepto
+    el nombre, de cualquier usuario, incluido el propio admin.
+
+    :param conn:
+                Objeto sql para realizar las conexiones a la base de datos estipulada
+    :param c:
+                Cursos de la conexión sql
+    :param query:
+                Query que se encarga de realizar la operacion adecuada para la consulta sql
+    :param result:
+                Resultado de la consulta sql
+    :param error:
+                Cadena con el contenido del error que se ha producido
+    :param count:
+                Selector de modos para el template register
+    """
+
+
 
     try:
 
@@ -302,6 +378,39 @@ def modify():
 
 @route('/modify_done', method = 'POST')
 def modify_done():
+
+    """
+    Ruta que aparece tras realizar la modificación de algun usuario por parte del administrador, 
+    se encarga de manejar toda la información relativa al formulario rellenado a la hora 
+    de modificar la información de cualquier usuario.
+
+    :param name:
+                Cadena con el nombre introducido en el formulario
+    :param password:
+                Cadena con la contraseña introducida en el formulario
+    :param correo:
+                Cadena con el correo introducido en el formulario
+    :param dni:
+                Cadena con el dni introducido en el formulario
+    :param departamento:
+                Cadena con el departamento introducido en el formulario
+    :param categoria:
+                Cadena con la categoria introducida en el formulario
+    :param conn:
+                Objeto sql para realizar las conexiones a la base de datos estipulada
+    :param c:
+                Cursos de la conexión sql
+    :param query:
+                Query que se encarga de realizar la operacion adecuada para la consulta sql
+    :param result:
+                Resultado de la consulta sql
+    :param error:
+                Cadena con el contenido del error que se ha producido
+    :param cont:
+                Selector de modos para el template register
+
+    """
+
 
     try:
         name = request.forms.get('name')
@@ -335,6 +444,25 @@ def modify_done():
 @route('/eliminate')
 def eliminate():
 
+    """
+    Ruta que solo se activa si eres admin y que sirve para eliminar a un usuario, incluido el propio admin.
+
+    :param conn:
+                Objeto sql para realizar las conexiones a la base de datos estipulada
+    :param c:
+                Cursos de la conexión sql
+    :param query:
+                Query que se encarga de realizar la operacion adecuada para la consulta sql
+    :param result:
+                Resultado de la consulta sql
+    :param error:
+                Cadena con el contenido del error que se ha producido
+    :param cont:
+                Selector de modos para el template register
+
+    """
+
+
     try:
 
 
@@ -362,6 +490,29 @@ def eliminate():
 
 @route('/eliminate_done', method='POST')
 def eliminate_done():
+
+    """
+    Ruta que aparece tras realizar la eliminación de algun usuario por parte del administrador, 
+    se encarga de manejar toda la información relativa al formulario mandado a la hora de
+    eliminar a cualquier usuario.
+
+    :param name:
+                Nombre del usuario que queremos borrar.
+    :param conn:
+                Objeto sql para realizar las conexiones a la base de datos estipulada
+    :param c:
+                Cursos de la conexión sql
+    :param query:
+                Query que se encarga de realizar la operacion adecuada para la consulta sql
+    :param result:
+                Resultado de la consulta sql
+    :param error:
+                Cadena con el contenido del error que se ha producido
+    :param cont:
+                Selector de modos para el template register
+
+    """
+
 
     try:
         name = request.forms.get('name')
--- source/runsql.py
+++ source/runsql.py
@@ -15,9 +15,30 @@ def databasecreate():
 
     print("Creando la base de datos");
 
+    # TABLA DE USUARIOS
+
     conn = sqlite3.connect('bottle.db')
     conn.execute("CREATE TABLE usuarios (nombre char(50) PRIMARY KEY NOT NULL, dni char(10) NOT NULL, pass char(20) NOT NULL, correo char(60) NOT NULL, departamento char(100), categoria char(5))")
     conn.execute("INSERT INTO usuarios (nombre, dni, correo, departamento, categoria, pass) VALUES ('admin','admin','admin@uca.es','Administracion','ADM', 'admin') ")
+    
+    # TABLA DE ASIGNATURAS
+
+    conn.execute("CREATE TABLE asignatura (id INTEGER PRIMARY KEY, nombre char(100))")
+    
+    while True:
+
+        consulta = input("Introduce una asignatura (n) para cancelar: ")
+        
+        if consulta == 'n':
+            break
+        else:
+            query = "INSERT INTO asignatura (nombre) VALUES ('{}')".format(consulta)
+            conn.execute(query)
+        
+    # TABLA DE PROF_ASIG
+
+    conn.execute("CREATE TABLE prof_asig (profesor char(50), asignatura char(100), foreign key(profesor) references usuarios(nombre) );")
+
     conn.commit()
     conn.close()
     print("\n Base de datos creada")    
